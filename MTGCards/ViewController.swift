f9c1ffb55f0015295503f066bd3af45124c73e8d
Joseph Smith <jmcsmith@gmail.com>
2018-03-03 13:40:08 -0500

    Initial Commit

---
+++ MTGCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MTGCards
+//
+//  Created by Joseph Smith on 3/3/18.
+//  Copyright © 2018 Robotic Snail Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
