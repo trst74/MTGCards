c12866ac04de8e7332a278f744d7a723eb5bc3b8
Joseph Smith <jmcsmith@gmail.com>
2020-04-09 18:14:31 -0400

    update to v.1.37.0 api version

--- MTGCards/Utilities/TcgPlayerApi.swift
+++ MTGCards/Utilities/TcgPlayerApi.swift
@@ -15,7 +15,7 @@ class TcgPlayerApi {
     private let clientSecret = "7A65BBC3-8B1A-43D8-998E-476949A4A6E8"
     
     private let tokenURL = "https://api.tcgplayer.com/token"
-    private let pricesURL = "http://api.tcgplayer.com/v1.27.0/pricing/product/"
+    private let pricesURL = "http://api.tcgplayer.com/v1.37.0/pricing/product/"
     private var token: TcgToken? = nil
     
     private func getToken( completion: @escaping (_ success: Bool) -> Void) {
